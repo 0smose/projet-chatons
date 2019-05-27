6a0118d5405fc557ffe2355786e5102d40fd14e8
Jahlawa <belabbes.l@hotmail.fr>
2019-05-27 14:00:45 +0200

    seed complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Item.destroy_all
+
+20.times do 
+
+	user = User.create!(
+		email: Faker::Internet.email,
+		password: Faker::Internet.password)
+
+	item = Item.create!(
+		title: Faker::Lorem.sentence,
+		description: Faker::Lorem.paragraph_by_chars(152, false),
+		price: rand(1..1000),
+		image_url: Faker::LoremFlickr.image("200x300"))
+end
