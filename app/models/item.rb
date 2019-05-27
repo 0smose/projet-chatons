a5c5fa7e3d327f5ffb2d259f5dda56afd6359946
fralps <francois.loupias@orange.fr>
2019-05-27 12:43:52 +0200

    Item table added

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+
+	# --------------- Items validates ------------------
+	validates :title, presence: true, length: { in: 5..50 }
+	validates :description, presence: true, length: { in: 5..240 }
+	validates :price, presence: true, numericality: { greater_than: 0.0 }
+	validates :image_url, presence: true
+end
