3c9cb67899a69f87c752a09dd2ee33d12cb392d8
CaroFG <carolina.ferreira131@gmail.com>
2019-05-28 14:34:40 +0200

    card total amount method and remove item from cart done

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,24 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_many :cart_items
 	has_many :items, through: :cart_items
+
+#
+	def total_cart_price
+		@cart = Cart.find(params[:id])
+		#We set a counter at 0
+		total = 0
+		#We iterate through the items in the cart
+		@cart.items.each do |item|
+		#We add the price item at the counter at each iteration of the loop,
+		#to obtain the total price
+			total+= item.price
+		end
+	end
+
+
+
+
+
+
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,11 @@
 class CartItem < ApplicationRecord
 	belongs_to :cart
 	belongs_to :item
+
+	def destroy
+		@cart_item = CartItem.where(cart_id: @cart.id, item_id: @item.id)[0]
+		@cart_item.destroy
+	end
 end
+
+
