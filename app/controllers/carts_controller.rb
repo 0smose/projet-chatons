389e226ac6f8ece20d0a099ab31aeeb8652e0a80
fralps <francois.loupias@orange.fr>
2019-05-28 16:02:06 +0200

    Cart button OK, display of buttons OK

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,8 @@
 class CartsController < ApplicationController
+
+	# Before action method in order to display the cart button only to authenticate user
+	before_action :authenticate_user!
+
 	def show
 		@cart = Cart.find(params[:id])
 	end
