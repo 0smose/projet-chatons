11f5f7c2113bd4a11c0f4cad4aac83677fa92665
CaroFG <carolina.ferreira131@gmail.com>
2019-05-29 21:24:57 +0200

    text and button alignment in carts show, add title to order history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_29_171540) do
 
   # These are extensions that must be enabled in order to support this database
@@ -87,12 +86,9 @@ ActiveRecord::Schema.define(version: 2019_05_29_171540) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.string "first_name"
     t.string "last_name"
-=======
     t.boolean "is_admin", default: false
->>>>>>> development
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
