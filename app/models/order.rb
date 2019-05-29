14221df742318c925faf707962bcf12d060ebbcb
Toww <th.guilmin@gmail.com>
2019-05-29 20:10:02 +0200

    Admin mailer OK

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,5 +9,6 @@ class Order < ApplicationRecord
 	# This method calls the order_email method from user_mailer, passing the order as an argument
 	def order_send
 		UserMailer.order_email(self).deliver_now
+		AdminMailer.order_email(self).deliver_now
 	end
 end
