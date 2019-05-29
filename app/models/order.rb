7731a65f9643adc076141d9b11c86fc45a49c221
Toww <th.guilmin@gmail.com>
2019-05-29 14:18:40 +0200

    Mailjer / Mailjet basic config OK

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :order_items
 	has_many :items, through: :order_items
+
+	# When an order si created, we call the method order_send
+	after_create :order_send
+
+	# This method calls the order_email method from user_mailer, passing the order as an argument
+	def order_send
+		puts "*"*100
+		UserMailer.order_email(self).deliver_now
+		puts "*"*100
+	end
 end
