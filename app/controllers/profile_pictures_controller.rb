d85e3218f41ab007cc31aebceea5efd580e434d4
CaroFG <carolina.ferreira131@gmail.com>
2019-05-29 17:30:14 +0200

    edit user view ok, controller wip

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,2 +1,8 @@
 class ProfilePicturesController < ApplicationController
+
+	 def create
+  	@user = User.find(params[:user_id])
+  	@user.profile_picture.attach(params[:profile_picture])
+  	redirec_to user_path(@user)
+  end
 end
