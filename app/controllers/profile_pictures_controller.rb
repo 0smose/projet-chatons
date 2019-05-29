43e1d0b85d9ed84e0487ca6d7a88b317ad3ad673
Toww <th.guilmin@gmail.com>
2019-05-29 23:03:33 +0200

    Fixes edit and slugs

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,8 +1,8 @@
 class ProfilePicturesController < ApplicationController
 
 	 def create
-  	@user = User.find(params[:user_id])
+  	@user = User.friendly.find(params[:user_id])
   	@user.profile_picture.attach(params[:profile_picture])
-  	redirect_to user_path(@user)
+  	redirect_to edit_user_path(@user.slug)
   end
 end
