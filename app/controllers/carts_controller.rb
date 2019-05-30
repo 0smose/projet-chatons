65257f77a95084c26ad4c37ce56c20f446015c77
Toww <th.guilmin@gmail.com>
2019-05-30 21:30:03 +0200

    cart item count ok, destroy WIP

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,6 +5,12 @@ class CartsController < ApplicationController
 
 	def show
 		@cart = Cart.find(params[:id])
+
+		# We want to show the items in the cart only once, so we create a new array
+		# without the duplicate entries
+		@cart_items = @cart.items
+		@cart_items_unique = @cart_items.uniq
+
 	end
 
 	def dont_show
