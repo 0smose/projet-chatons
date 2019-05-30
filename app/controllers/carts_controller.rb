9255bb1e7dfa9732d6d069357ec09910377ac6cc
CaroFG <carolina.ferreira131@gmail.com>
2019-05-30 17:35:41 +0200

    cart for visitors wip

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,18 @@
 class CartsController < ApplicationController
-
+	helper_method :current_or_guest_user
 	# Before action method in order to display the cart button only to authenticate user
-	before_action :authenticate_user!
 	before_action :dont_show
 
 	def show
 		@cart = Cart.find(params[:id])
+		puts "*"*60
+		puts @cart.user
+		puts guest_user
 	end
 
 	def dont_show
 		@cart = Cart.find(params[:id])
-		redirect_to root_path, alert: "It's not your order !" unless @cart.user == current_user
+		redirect_to root_path, alert: "It's not your order !" unless @cart.user == current_or_guest_user
 	end
 
 
